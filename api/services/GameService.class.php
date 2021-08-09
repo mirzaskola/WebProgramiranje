de54371e4e2211a3dd0dc10cfe8ada99944f5eeb
mirzaskola <mirza.kurtovic@stu.ibu.edu.ba>
2021-08-09 13:22:52 +0200

    Game service

--- api/services/GameService.class.php
+++ api/services/GameService.class.php
@@ -1,1 +1,34 @@
-<?php?>
+<?php
+
+require_once dirname(__FILE__). '/BaseService.class.php';
+require_once dirname(__FILE__).'/../dao/GameDao.class.php';
+
+class GameService extends BaseService{
+
+  private $categoryDao;
+
+  public function __construct(){
+    $this->dao = new GameDao();
+  }
+
+
+  public function get_game($search, $offset, $limit, $order, $total = FALSE){
+    return $this->dao->get_game($search, $offset, $limit, $order, $total);
+  }
+
+
+  public function add_game($game){
+
+      $data= [
+        "game_name" => $game['game_name'],
+        "description" => $game['description'],
+        "image" => $game['image'],
+        "rating" =>$game['rating']
+      ];
+
+
+       return parent::add($data);
+
+  }
+}
+?>
